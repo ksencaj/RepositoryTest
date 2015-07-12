e4c8b71c9742ee1ff0b1f369ae63722ad3c0ef83
ksencaj <ksencajfx@gmail.com>
2015-07-12 22:58:06 +0200

    RepoCreator created repository from template . The template can be found at https://github.com/Zoltu/Templates.NuGet/

---
+++ magic_tests_project_magic_code_project_name_magic_magic/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("magic_tests_project_magic_code_project_name_magic_magic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("magic_company_magic_code_project_name_magic_magic")]
+[assembly: AssemblyProduct("magic_product_magic_code_project_name_magic_magic")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
